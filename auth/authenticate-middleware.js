2dd54ea37d36b0cee465d73849091bed62ea9dce
unknown <dj.zaragoza@icloud.com>
2019-08-31 11:42:16 -0700

    added middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,45 @@
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+const db = require('../database/dbUsers');
+const bcrypt = require('bcrypt');
+
+//middlware to validate if the cookie is existing and valid
+
+function verifyLoggedUser(req, res, next) {
+  if(req.session && req.session.user) {
+    next();
+  } else{
+    res.status(400).json({ you: 'stay right there!'});
+  }
+}
+
+//middleware to verify new user and create the cookie
+
+async function createCookie(req, res,next) {
+  const {username, password} = req.body;
+  try {
+    if(username && password) {
+      const [user] = await db.findByUsername(username);
+
+      if(user && bcrypt.compareSync(password, user.password)) {
+        console.log(user)
+        //req.session.user = user.username;
+        req.params.username = user.username
+        next();
+      } else {
+        res.status(500).json({message: "Invalid Credentials"})
+      }
+    } else {
+      res.status(403).json({message: "Provide credentials to login"})
+    }
+  }
+  catch(err) {
+    res.status(500).json(err.message);
+  }
+}
+
+module.exports = {
+    verifyLoggedUser,
+    createCookie
 };
